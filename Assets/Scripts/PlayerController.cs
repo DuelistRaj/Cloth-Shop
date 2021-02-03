dbe98fd7cc3f35c2f91df5d3b477f4f275d04a56
duelistraj <duelistrahul@gmail.com>
2021-02-03 19:36:00 +0530

    Added Player Movement and Idle Animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,5 +4,63 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    
+    [SerializeField]
+    private float moveSpeed = 5;
+
+    private bool isMoving;
+
+    private Vector2 input;
+
+    private Animator animator;
+
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        if(!isMoving)
+        {
+            input.x = Input.GetAxisRaw("Horizontal");
+            input.y = Input.GetAxisRaw("Vertical");
+
+            if (input.x != 0)
+                input.y = 0;
+
+            if (input != Vector2.zero)
+            {
+                spriteRenderer.flipX = false;
+
+                animator.SetFloat("moveX", input.x);
+                animator.SetFloat("moveY", input.y);
+
+                if (input.x >= 0)
+                    spriteRenderer.flipX = true;
+
+                var targetPos = transform.position;
+                targetPos.x += input.x;
+                targetPos.y += input.y;
+
+                StartCoroutine(Move(targetPos));
+            }
+        }
+    }
+
+    IEnumerator Move(Vector3 targetPos)
+    {
+        isMoving = true;
+
+        while((targetPos - transform.position).sqrMagnitude >= Mathf.Epsilon)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+        transform.position = targetPos;
+
+        isMoving = false;
+    }
 }
